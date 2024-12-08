390a118222c0e9e1756fd9da88a8cc223b0108e0
Julaczos <julaczosoo@gmail.com>
2024-12-08 09:28:31 +0100

    Update poseUtils.js

--- poseUtils.js
+++ poseUtils.js
@@ -1,8 +1,8 @@
 function calculateAngle(A, B, C) {
     const radians = Math.atan2(C.y - B.y, C.x - B.x) - Math.atan2(A.y - B.y, A.x - B.x);
     let angle = Math.abs(radians * 180.0 / Math.PI);
-    if (angle > 180) {
-        angle = 360 - angle;
+    if (angle > 190) {
+        angle = 360 - angle + 10;
     }
     return angle;
 }
