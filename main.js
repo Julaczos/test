bdfca0dcfe81349ad8ec64c7541748c0b0061aad
Julaczos <julaczosoo@gmail.com>
2024-12-08 09:21:16 +0100

    Update main.js

--- main.js
+++ main.js
@@ -53,9 +53,9 @@ function updateSquatCounter(poseLandmarks) {
 
     const averageKneeAngle = (leftKneeAngle + rightKneeAngle) / 2;
 
-    if (averageKneeAngle < 155 && !isSquatting) {
+    if (averageKneeAngle < 165 && !isSquatting) {
         isSquatting = true; 
-    } else if (averageKneeAngle > 160 && isSquatting) {
+    } else if (averageKneeAngle > 170 && isSquatting) {
         squatCount++;
         isSquatting = false;
         document.getElementById("squatCounter").innerText = `Przysiady: ${squatCount}`;
